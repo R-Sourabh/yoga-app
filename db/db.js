9f696563593de51784c4c1714573ed533bc1323c
Sourabh Raghuwanshi <rsourabh002@gmail.com>
2022-12-14 18:11:06 +0530

    Fixed env

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const Pool = require("pg").Pool;
 // const config = require("./dbconfig")
+require('dotenv').config()
 let connectionString = process.env.DB_URI;
 const pool = new Pool({ connectionString: connectionString });
 pool.connect();
